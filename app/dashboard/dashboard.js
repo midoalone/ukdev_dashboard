1130990326f5f3a74786a8f6b0c9fb71e64aea47
midoalone <mido_alone2001@yahoo.com>
2017-11-05 18:38:01 +0200

    Add new page + add widget

--- app/dashboard/dashboard.js
+++ app/dashboard/dashboard.js
@@ -14,30 +14,37 @@ angular.module('myApp.dashboard', ['ngRoute', 'gridstack-angular'])
         });
     }])
 
-    .controller('dashboardCtrl', function ($scope, $routeParams, $log, $http) {
+    .controller('dashboardCtrl', function ($scope, $routeParams, $log, $http, $location, $timeout) {
 
         // console.log($routeParams.pageName);
 
+        $('[data-toggle="tooltip"]').tooltip();
+
         // Pages
         var pagesText = localStorage.getItem("pages");
-        var pages = JSON.parse(pagesText);
+        var pages = angular.fromJson(pagesText);
+        
+        // console.log(pages);
 
         if (pages === null) {
             pages = [{title: "Dashboard", slug: "main"}, {title: "E-Commerce", slug: "commerce"}];
-            localStorage.setItem("main", JSON.stringify([]));
-            localStorage.setItem("pages", JSON.stringify(pages));
+            localStorage.setItem("main", angular.toJson([]));
+            localStorage.setItem("pages", angular.toJson(pages));
         }
 
         $scope.pages = pages;
 
         $scope.addPage = function () {
+            var title = "Page " + ($scope.pages.length + 1);
+            var pageSlug = slug(title);
+
             $scope.pages.push({
-                title: $scope.page.title,
-                slue: $scope.page.slug
+                title: title,
+                slug: pageSlug
             });
 
-            localStorage.setItem($scope.page.slug, JSON.stringify([]));
-            localStorage.setItem("pages", JSON.stringify($scope.pages));
+            localStorage.setItem(pageSlug, angular.toJson([]));
+            localStorage.setItem("pages", angular.toJson($scope.pages));
         };
 
         // Edit page
@@ -50,6 +57,9 @@ angular.module('myApp.dashboard', ['ngRoute', 'gridstack-angular'])
                 var $this = $($event.target);
                 $this.attr("readonly", true);
                 var oldSlug = $page.slug;
+                var newSlug = slug($this.val());
+
+                if(oldSlug === newSlug) return;
 
                 var $pages = [];
                 $(pages).each(function (i, page){
@@ -60,7 +70,7 @@ angular.module('myApp.dashboard', ['ngRoute', 'gridstack-angular'])
 
                     if($page.slug === page.slug){
                         pageObject.title = $this.val();
-                        pageObject.slug = slug($this.val());
+                        pageObject.slug = newSlug;
 
                         var pageWidgets = localStorage.getItem(oldSlug);
                         localStorage.setItem(pageObject.slug, pageWidgets);
@@ -70,7 +80,10 @@ angular.module('myApp.dashboard', ['ngRoute', 'gridstack-angular'])
                     $pages.push(pageObject);
                 });
 
-                localStorage.setItem("pages", JSON.stringify($pages));
+                localStorage.setItem("pages", angular.toJson($pages));
+                $scope.pages = pages;
+
+                $location.url("/dashboard/" + newSlug);
             }
         };
 
@@ -93,62 +106,75 @@ angular.module('myApp.dashboard', ['ngRoute', 'gridstack-angular'])
             return $routeParams.pageName === $slug;
         };
 
+        // Save next & previous pages
+        var nextPage=null,
+            prevPage=null,
+            currentIndex=null;
+
+        $(pages).each(function (i, $page){
+            if($page.slug === $routeParams.pageName) {
+                currentIndex = i;
+                if(currentIndex !== 0) prevPage = pages[i-1];
+                if(currentIndex < pages.length) nextPage = pages[i+1];
+            }
+        });
+
+        // Previous Page
+        $scope.previousPage = function (){
+            if(prevPage){
+                $location.url("/dashboard/"+prevPage.slug);
+            }
+        };
+
+        // Next Page
+        $scope.nextPage = function (){
+            if(nextPage){
+                $location.url("/dashboard/"+nextPage.slug);
+            }
+        };
+
         // Widgets
         var widgetsText = localStorage.getItem($routeParams.pageName);
-        var widgets = JSON.parse(widgetsText);
+        var widgets = angular.fromJson(widgetsText);
 
         if(widgets !== null){
             $scope.widgets = widgets;
         }else{
-            $scope.widgets = [
-                {x: 0, y: 0, width: 3, height: 1, id: 'weather'},
-                {x: 3, y: 0, width: 3, height: 1, id: 'chart'}
-            ];
+            $scope.widgets = [];
         }
 
-        // http://localhost:8000/json/weather.json
-
-        localStorage.setItem($routeParams.pageName, JSON.stringify($scope.widgets));
+        localStorage.setItem($routeParams.pageName, angular.toJson($scope.widgets));
 
         // Initialize gridstack
         $scope.options = {
-            cellHeight: 100,
+            cellHeight: 150,
             verticalMargin: 10
         };
 
-        $('.grid-stack').gridstack($scope.options);
+        var gridStack = $('.grid-stack').gridstack($scope.options);
         var grid = $('.grid-stack').data('gridstack');
 
         // Load saved data
         _.each($scope.widgets, function (node) {
-            grid.addWidget($('<div data-id="'+node.id+'"><div class="grid-stack-item-content" /><div/>'),
-                node.x, node.y, node.width, node.height);
-
-            getWidgetContent(node.id);
+            getWidgetContent(node);
         }, this);
 
         // Load widgets content
-        function getWidgetContent($id){
-            $http.get('http://localhost:8000/json/'+$id+'.json').success(function (data) {
-                $('[data-id="'+$id+'"]').find('.grid-stack-item-content').html(data.content);
+        function getWidgetContent(node){
+            grid.addWidget($('<div data-id="'+node.id+'"><div class="grid-stack-item-content" /><i class="glyphicon glyphicon-trash remove-widget"></i><div/>'),
+                node.x, node.y, node.width, node.height);
+
+            $http.get('http://localhost:8000/json/'+node.id+'.json').success(function (data) {
+                $('[data-id="'+node.id+'"]').find('.grid-stack-item-content').html(data.content);
             });
         }
 
-        // Show widget on added
-        $('.grid-stack').on('added', function(event, item) {
-            $log.log("onItemAdded item: " + item);
-
-            var $item = $(item.el[0]);
-            $log.log($item.data("id"));
-
-        });
-
         // Save on change
-        $('.grid-stack').on('change', function(event, items) {
+        gridStack.on('change', function(event, items) {
             var $items = _.map($('.grid-stack > .grid-stack-item:visible'), function (el) {
                 el = $(el);
                 var node = el.data('_gridstack_node');
-                console.log(node);
+                // console.log(node);
                 return {
                     x: node.x,
                     y: node.y,
@@ -158,51 +184,61 @@ angular.module('myApp.dashboard', ['ngRoute', 'gridstack-angular'])
                 };
             }, this);
 
-            localStorage.setItem($routeParams.pageName, JSON.stringify($items));
+            localStorage.setItem($routeParams.pageName, angular.toJson($items));
         });
 
         $scope.addWidget = function () {
-            var newWidget = {x: 0, y: 0, width: 1, height: 1};
+            var newWidget = {x: 0, y: 0, width: 3, height: 1, id: $scope.widgetSelector};
             $scope.widgets.push(newWidget);
+            localStorage.setItem($routeParams.pageName, angular.toJson($scope.widgets));
+
+            getWidgetContent(newWidget);
         };
 
-        $scope.removeWidget = function (w) {
-            var index = $scope.widgets.indexOf(w);
-            $scope.widgets.splice(index, 1);
-        };
+        // Remove Widget
+        $('body')
+            .off('click', '.remove-widget')
+            .on('click','.remove-widget', function (){
+            var item = $(this).closest('.grid-stack-item');
+            if(item){
+                grid.removeWidget(item);
+            }
+        });
 
-        $scope.onDragStart = function (event, ui) {
-            $log.log("onDragStart event: " + event + " ui:" + ui);
-        };
+        // Move widgets
+        var movableItem;
+        localStorage.removeItem("MovableWidget");
 
-        $scope.onDragStop = function (event, ui) {
-            $log.log("onDragStop event: " + event + " ui:" + ui);
-        };
+        gridStack.on("dragstart", function (event, item){
+            var $item = $(event.target),
+                width = $item.data("gs-width"),
+                height = $item.data("gs-height"),
+                id = $item.data("id");
 
-        $scope.onResizeStart = function (event, ui) {
-            $log.log("onResizeStart event: " + event + " ui:" + ui);
-        };
+            localStorage.setItem("MovableWidget", angular.toJson({x:0,y:0,width:width, height:height, id:id}));
+            movableItem = item;
+        });
 
-        $scope.onResizeStop = function (event, ui) {
-            $log.log("onResizeStop event: " + event + " ui:" + ui);
-        };
+        gridStack.on("dragstop", function (){
+            localStorage.removeItem("MovableWidget");
+        });
 
-        $scope.onItemAdded = function (item, api) {
-            // $log.log("onItemAdded item: " + item);
-            // $log.log(item);
+        $scope.pageHovered = function (pageSlug){
+            var widget = angular.fromJson(localStorage.getItem("MovableWidget"));
 
-            var $item = $(item.el[0]);
+            if(widget !== null){
+                // Add to target page
+                var $targetWidgets = angular.fromJson(localStorage.getItem(pageSlug));
+                if($targetWidgets !== null){
+                    $targetWidgets.push(widget);
+                    localStorage.setItem(pageSlug, angular.toJson($targetWidgets));
+                }
 
-            item.api = api;
-            item.id = $item.attr('id');
+                $timeout(function (){
+                    $location.url("/dashboard/" + pageSlug);
+                }, 100);
 
-            $http.get(api).success(function (data) {
-                $item.find('.grid-stack-item-content').html(data.content);
-            });
+            }
+        }
 
-        };
-
-        $scope.onItemRemoved = function (item) {
-            $log.log("onItemRemoved item: " + item);
-        };
     });
